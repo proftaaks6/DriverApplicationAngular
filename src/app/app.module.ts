4e37822bd129253e56168454346cd9242d123e24
Unknown <m.hennen@student.fontys.nl>
2019-05-14 13:52:16 +0200

    folder sttructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceComponent
   ],
   imports: [
     BrowserModule,
