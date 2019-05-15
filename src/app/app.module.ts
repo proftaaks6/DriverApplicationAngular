6acc22760744379a8bd391f0ca32c8658a4d44f0
Unknown <m.hennen@student.fontys.nl>
2019-05-15 16:22:13 +0200

    facturen ophalen + betalen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InvoiceComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
